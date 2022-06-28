4bc085e0035757b55942a18d4d4bc4b68cb362c9
panagiotis <panagiotisdrakatos@gmail.com>
2022-06-28 15:50:58 +0300

    adding modules

---
+++ adrestus-consensus/src/main/java/io/Adrestus/consensus/App.java
@@ -0,0 +1,13 @@
+package io.Adrestus.consensus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
