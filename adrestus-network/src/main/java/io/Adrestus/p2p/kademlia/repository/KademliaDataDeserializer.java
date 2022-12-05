b834acdfa1239cc4151460e4e195410f40f7b0f5
panagiotis <panagiotisdrakatos@gmail.com>
2022-12-05 21:24:00 +0200

    Fixing a small bug in network seriliazation

--- adrestus-network/src/main/java/io/Adrestus/p2p/kademlia/repository/KademliaDataDeserializer.java
+++ adrestus-network/src/main/java/io/Adrestus/p2p/kademlia/repository/KademliaDataDeserializer.java
@@ -24,7 +24,6 @@ public class KademliaDataDeserializer implements JsonDeserializer<KademliaData> 
         kademliaData.setHash(resourceJson.get("hash").getAsString());
         JsonObject blsobj = resourceJson.get("address_data").getAsJsonObject().get("validator_bl_s_public_key").getAsJsonObject();
         String address = resourceJson.get("address_data").getAsJsonObject().get("address").getAsString();
-        String IP = resourceJson.get("address_data").getAsJsonObject().get("ip").getAsString();
         BigInteger ecdsaKey = resourceJson.get("address_data").getAsJsonObject().get("e_c_d_s_a_public_key").getAsBigInteger();
         JsonObject ecdsaSignatureObj = resourceJson.get("address_data").getAsJsonObject().get("e_c_d_s_a_signature").getAsJsonObject();
         SecurityAuditProofs securityAuditProofs = new SecurityAuditProofs(
